38d293333e00bddc161a8f44169be25944075200
jackjackE <tmddjs210@naver.com>
2020-07-20 20:28:22 +0900

    init

---
+++ FirebaseMVVM/app/src/main/java/com/mtjin/firebasemvvm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mtjin.firebasemvvm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mtjin.firebasemvvm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
