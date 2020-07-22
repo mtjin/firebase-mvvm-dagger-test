533fb604ea2acf80ea74c6a8c39bfc39a4864463
Jin Seung Eon <tmddjs210@naver.com>
2020-07-22 12:21:32 +0900

    rm

--- FirebaseMVVM/app/src/main/java/com/mtjin/firebasemvvm/di/AppSubComponentsModule.kt
+++
@@ -1,7 +0,0 @@
-package com.mtjin.firebasemvvm.di
-
-import com.mtjin.firebasemvvm.ui.MainComponent
-import dagger.Module
-
-@Module(subcomponents = [MainComponent::class])
-class AppSubComponentsModule
